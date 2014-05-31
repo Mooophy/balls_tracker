4fbc6b412b613d6e1ad4ba0ff30e3067624876be
mooophy <MoophyAlan@gmail.com>
2014-05-31 13:38:07 +1200

    added iteration for multiple balls tracking 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -27,32 +27,35 @@ int main( )
         //! raw image
         cv::Mat raw,gray, hsv, blue,red,green, output;
         cap >> raw;
-/*
 
         cv::cvtColor(raw,hsv,cv::COLOR_BGR2HSV);
         cv::imshow("hsv",hsv);
 
-        //cv::inRange(hsv,cv::Scalar(100,100,100),cv::Scalar(120,255,255), blue);
-        //cv::imshow("blue",blue);
+//        cv::inRange(hsv,cv::Scalar(100,100,100),cv::Scalar(120,255,255), blue);
+//        cv::imshow("blue",blue);
 
-        //! red range [150,100,100],[179,255,255];
-        cv::inRange(hsv,cv::Scalar(150,100,100),cv::Scalar(179,255,255), red);
+//        //! red range [150,100,100],[179,255,255];
+//        cv::inRange(hsv,cv::Scalar(150,100,100),cv::Scalar(179,255,255), red);
+
+        //cv::imshow("red", red);
+        //! green range [30,100,100],[60,255,255];
+        cv::inRange(hsv,cv::Scalar(30,100,100),cv::Scalar(60,255,255), green);
+//        cv::cvtColor(raw,gray, cv::COLOR_BGR2GRAY);
+//        cv::imshow("gray", gray);
+
+        cv::GaussianBlur( green, green, cv::Size(9, 9), 2, 2 );
 
 
+        cv::HoughCircles(green,circles,CV_HOUGH_GRADIENT, 1, 100,50,12,40);
 
-        cv::imshow("red", red);
-*/
-        cv::cvtColor(raw,gray, cv::COLOR_BGR2GRAY);
-
-        cv::GaussianBlur( gray, gray, cv::Size(9, 9), 2, 2 );
-
-
-        cv::HoughCircles(gray,circles,CV_HOUGH_GRADIENT, 3, 10000,100,20,40);
-
+        std::cout << "size = " << circles.size() << std::endl;
         if(!circles.empty() && circles[0][2] > 10 && circles[0][2] < 80)
         {
             std::cout << *circles.begin() << std::endl;
-            cv::circle(raw,cv::Point(circles[0][0],circles[0][1]), 45, cv::Scalar(0),3);
+
+            //cv::circle(raw,cv::Point(circles[0][0],circles[0][1]), 45, cv::Scalar(0),3);
+            for(unsigned i = 0; i != circles.size(); ++i)
+                cv::circle(raw,cv::Point(circles[i][0],circles[i][1]), 45, cv::Scalar(0),3);
         }
 
         //cv::circle(raw,cv::Point(0,0),50,cv::Scalar(0));
